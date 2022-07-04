33b70f8669277a448f54db5d3509dfb8e694eb3d
cmb1197 <cmb1197@hotmail.com>
2022-07-04 18:41:05 -0500

    Formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component, VERSION } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  formulario: FormGroup;
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.formulario = this.fb.group({
+      nombres: [, Validators.compose([Validators.required])],
+      edad: ['', Validators.compose([Validators.required])],
+      sexo: ['', Validators.compose([Validators.required])],
+      comentario: ['', Validators.compose([])],
+    });
+  }
+
+  aceptar() {
+    console.log(this.formulario);
+
+    if (this.formulario.valid) {
+      alert('Okk');
+    }
+  }
 }
